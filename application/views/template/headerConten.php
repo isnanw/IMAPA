b0f171a4ce82bbeb83fa3ded017e67aa710a94fc
Isnan Wahyudi <isnan@gin.co.id.com>
2020-02-11 23:21:18 +0700

    view menu

--- application/views/template/headerConten.php
+++ application/views/template/headerConten.php
@@ -1,24 +1,30 @@
 <div class="pcoded-content">
-            <!-- Page-header start -->
-            <div class="page-header">
-                <div class="page-block">
-                    <div class="row align-items-center">
-                        <div class="col-md-8">
-                            <div class="page-header-title">
-                                <h5 class="m-b-10"><?= $title ?></h5>
-                                <p class="m-b-0">Lorem Ipsum is simply dummy text of the printing</p>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <ul class="breadcrumb">
-                                <li class="breadcrumb-item">
-                                    <a href="index.html"> <i class="fa fa-home"></i> </a>
-                                </li>
-                                <li class="breadcrumb-item"><a href="#!"><?= $title ?></a>
-                                </li>
-                            </ul>
-                        </div>
+    <!-- Page-header start -->
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="page-header-title">
+                        <h5 class="m-b-10"><?= $title ?></h5>
+                        <p class="m-b-0">Lorem Ipsum is simply dummy text of the printing</p>
                     </div>
                 </div>
+                <div class="col-md-4">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.html"> <i class="fa fa-home"></i> </a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!"><?= $title ?></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <!-- Page-header end -->
+        </div>
+    </div>
+    <!-- Page-header end -->
+    <div class="pcoded-inner-content">
+        <div class="main-body">
+            <div class="page-wrapper">
+                <div class="page-body">
+                    <div class="row">
+                        <div class="col-sm-12">
